0cdf0859601ebdd9b55c0b0d36389b79dc71ddf6
Lukazovic <lucas10.vieira.lv@gmail.com>
2020-11-20 19:14:25 -0300

    Update app

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,9 @@
-import '../styles/globals.css'
-import '../styles/navbar.css'
+import '../styles/globals.css';
+import '../styles/navbar.css';
+import '../styles/Profile.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
