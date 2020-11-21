55e05dc18c628bd8bb40f664d53f41c0dc954e63
Lukazovic <lucas10.vieira.lv@gmail.com>
2020-11-21 15:24:12 -0300

    Add repositories component

---
+++ pages/_repositories.js
@@ -0,0 +1,41 @@
+const Repositories = ({ repositories }) => (
+  <section id="repositories">
+    {console.log(repositories[0])}
+    <div className="wrapper">
+      <h1 className="repositories-title">Repositories</h1>
+
+      <div className="repositories-container">
+        {repositories.map(repository => (
+          <div className="repository-card">
+            <div className="repository-card-top">
+              <a
+                className="repository-title"
+                href={repository.repoUrl}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {repository.name}
+              </a>
+              <p className="repository-description">{repository.description}</p>
+            </div>
+
+            <div className="repository-card-bottom">
+              <div className="repository-extra-info">
+                <p>{repository.language}</p>
+                <p>Stars: {repository.starsCount}</p>
+                <p>Watchers: {repository.watchersCount}</p>
+                <p>Forks: {repository.forksCount}</p>
+              </div>
+
+              <div className="repository-updated-info">
+                <p>Updated {repository.updatedDistance}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  </section>
+);
+
+export default Repositories;
