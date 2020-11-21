592676bf976594cd09b3b6626c003ac3182ee388
Lukazovic <lucas10.vieira.lv@gmail.com>
2020-11-21 19:27:31 -0300

    Add footer component

---
+++ pages/_footer.js
@@ -0,0 +1,28 @@
+const Footer = () => (
+  <footer id="footer">
+    <div className="wrapper">
+      <p>
+        To see this project's code click{' '}
+        <a
+          href="https://github.com/Lukazovic/github-profiles-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          here
+        </a>
+      </p>
+      <p>
+        Made by{' '}
+        <a
+          href="https://www.linkedin.com/in/vieira-lucas/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Lucas Vieira
+        </a>
+      </p>
+    </div>
+  </footer>
+);
+
+export default Footer;
