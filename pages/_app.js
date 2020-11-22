602b60f7ee7b22cfe26588a2f6e431a4d08665b9
Lukazovic <lucas10.vieira.lv@gmail.com>
2020-11-22 18:12:34 -0300

    Integrated new pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,10 @@
 import '../styles/globals.css';
 import '../styles/Navbar.css';
 import '../styles/Profile.css';
+import '../styles/ProfileLoading.css';
 import '../styles/Repositories.css';
 import '../styles/Footer.css';
+import '../styles/Search.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
