0939ea2cf88cda9448d75881d9c6d9089c5cb24e
Lukazovic <lucas10.vieira.lv@gmail.com>
2020-11-22 19:02:46 -0300

    Fix: Next-Config file to accepts more images domains

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 module.exports = {
   images: {
     deviceSizes: [320, 640, 768, 1024, 1600],
-    domains: ['avatars1.githubusercontent.com'],
+    domains: [
+      'avatars0.githubusercontent.com',
+      'avatars1.githubusercontent.com',
+      'avatars2.githubusercontent.com',
+      'avatars3.githubusercontent.com',
+      'avatars4.githubusercontent.com',
+      'avatars5.githubusercontent.com',
+    ],
   },
 };
