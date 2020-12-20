479fec488c650db3a3f678e1688bf60cb26bfa78
Lukazovic <lucas10.vieira.lv@gmail.com>
2020-12-19 23:32:16 -0300

    Integrated NProgress to all pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,6 @@
+import NProgress from 'nprogress';
+import { Router } from 'next/router';
+
 import '../styles/globals.css';
 import '../styles/Navbar.css';
 import '../styles/Profile.css';
@@ -6,8 +9,24 @@ import '../styles/Repositories.css';
 import '../styles/Footer.css';
 import '../styles/Search.css';
 
+Router.events.on('routeChangeStart', () => {
+  NProgress.start();
+});
+
+Router.events.on('routeChangeComplete', () => {
+  NProgress.done();
+});
+Router.events.on('routeChangeError', () => {
+  NProgress.done();
+});
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <link rel="stylesheet" href="/nprogress.css" type="text/css" />
+      <Component {...pageProps} />;
+    </>
+  );
 }
 
 export default MyApp;
