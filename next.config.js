73e0d4b0c2b2fc2f53cdb8f371ea7a4df7c6753f
Lukazovic <lucas10.vieira.lv@gmail.com>
2021-02-09 16:23:50 -0300

    fix: profile image not loading

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ module.exports = {
   images: {
     deviceSizes: [320, 640, 768, 1024, 1600],
     domains: [
+      'avatars.githubusercontent.com',
       'avatars0.githubusercontent.com',
       'avatars1.githubusercontent.com',
       'avatars2.githubusercontent.com',
